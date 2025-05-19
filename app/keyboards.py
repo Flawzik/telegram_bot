4624b3b33e2b96fee69d3d127aa80a6a72258fdd
Flawzik <79632599+Flawzik@users.noreply.github.com>
2025-05-19 06:33:30 +0300

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,16 +1,34 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
-main = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='Смотреть анкеты 💞'),KeyboardButton(text='Моя анкета 👤')],
+
+prof = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='1'), KeyboardButton(text='2')],
+        [KeyboardButton(text='3'), KeyboardButton(text='4')]
+    ],
+    resize_keyboard=True
+)
+
+# Основная клавиатура
+main = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Смотреть анкеты 💞'), KeyboardButton(text='Моя анкета 👤')],
         [KeyboardButton(text='Подписка 👑')]
-],
+    ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите пункт меню")
+    input_field_placeholder="Выберите пункт меню"
+)
 
-menu = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Оплата подписки',url='https://www.youtube.com/watch?v=HYi4a2eZL50')]
-    ])
+# Клавиатура для подписки
+menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплата подписки', url='https://www.youtube.com/watch?v=HYi4a2eZL50')]
+    ]
+)
 
+# Клавиатура для профиля (исправлено название и параметры)
+
+'''     
 shalavi=['Shluha_1','shluha_2']
 
 async def inline_shalavi():
@@ -18,8 +36,4 @@ async def inline_shalavi():
     for shalavi in shalavi:
         keyboard.add(InlineKeyboardButton(text=shalavi))
     return keyboard.adjust(2).as_markup    
-
-prof = ReplyKeyboardMarkup(keyboard_prof=[
-        [KeyboardButton(text='1'),KeyboardButton(text='2'),[KeyboardButton(text='3')],[KeyboardButton(text='4')]]
-],
-    resize_keyboard_prof=True)
+  '''
